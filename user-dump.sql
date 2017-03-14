888f80d4319fd019c86c55d053f75c89ee409e47
ParkerH96 <parker.a.householder@gmail.com>
2017-03-14 13:20:24 -0400

    Create user-dump.sql
    This is just a simple database for a database with a single table called USER that stores a user_id, first name, middle name, last name, dob, and gender.

---
+++ user-dump.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS users;
+CREATE DATABASE users;
+
+DROP TABLE IF EXISTS USER;
+CREATE TABLE USER
+(
+  user_id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(64) NOT NULL,
+  middle_name varchar(64), 
+  last_name varchar(64) NOT NULL,
+  dob date NOT NULL,
+  gender varchar(64) NOT NULL,
+  PRIMARY KEY (user_id)
+);
